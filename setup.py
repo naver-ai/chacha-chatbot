aa9166e85f128f44fc01307421fb7cd292d92230
Young-Ho Kim <yghokim@gmail.com>
2023-07-07 21:55:59 +0900

    Added setup script.

---
+++ setup.py
@@ -0,0 +1,20 @@
+from os import path, getcwd
+
+from dotenv import find_dotenv, set_key
+
+#Create env files if not exist ========================================================
+
+env_file = find_dotenv()
+if not path.exists(env_file):
+    env_file = open(path.join(getcwd(), '.env'), 'w')
+    env_file.close()
+    env_file = find_dotenv()
+
+while True:
+    openai_api_key = input("Enter OpenAI API Key: ").strip()
+
+    if openai_api_key is not None and len(openai_api_key) > 0:
+        set_key(env_file, "OPENAI_API_KEY", openai_api_key)
+        break
+
+print("Wrote .env file.")
