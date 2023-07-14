ae8a0ff9909d5f3b91398d1406f74580d8ce9a28
Young-Ho Kim <yghokim@younghokim.net>
2023-07-14 14:15:40 +0900

    Added functions feature.

--- core/generators/base.py
+++ core/generators/base.py
@@ -1,6 +1,8 @@
+import json
 import os
 from os import getcwd, path
 from asyncio import to_thread
+from typing import Awaitable, Any, Callable
 
 import yaml
 
@@ -107,7 +109,8 @@ class ChatGPTResponseGenerator(ResponseGenerator):
                  model: str = ChatGPTModel.GPT_4.value,
                  base_instruction: str | None = None,
                  initial_user_message: str | None = None,
-                 params: ChatGPTParams | None = None
+                 params: ChatGPTParams | None = None,
+                 function_handler: Callable[[str, dict | None], Awaitable[Any]] | None = None
                  ):
 
         self.model = model
@@ -117,6 +120,8 @@ class ChatGPTResponseGenerator(ResponseGenerator):
         self.initial_user_message = initial_user_message
 
         self.base_instruction = base_instruction if base_instruction is not None else "You are a ChatGPT assistant that is empathetic and supportive."
+
+        self.function_handler = function_handler
 
     def get_instruction(self) -> str | None:
         return self.base_instruction
@@ -149,7 +154,31 @@ class ChatGPTResponseGenerator(ResponseGenerator):
         if top_choice.finish_reason == 'stop':
             response_text = top_choice.message.content
             return response_text, None
+        elif top_choice.finish_reason == 'function_call':
+            function_call_info = top_choice["message"]["function_call"]
+            function_name = function_call_info["name"]
+            function_args = json.loads(function_call_info["arguments"])
+            print(f"Call function - {function_name} ({function_args})")
+            function_call_result = await self.function_handler(function_name, function_args)
+            dialogue_with_func_result = dialogue_converted + [{
+                "role": "function",
+                "name": function_name,
+                "content": function_call_result
+            }]
+            new_result = await to_thread(openai.ChatCompletion.create,
+                                     model=self.model,
+                                     messages=dialogue_with_func_result,
+                                     **self.gpt_params.to_params()
+                                     )
+            top_choice = new_result.choices[0]
+            if top_choice.finish_reason == 'stop':
+                response_text = top_choice.message.content
+                return response_text, None
+            else:
+                print("Shouldn't be here")
+
         else:
+            print(top_choice)
             raise Exception(f"ChatGPT error - {top_choice.finish_reason}")
 
 
--- core/openai.py
+++ core/openai.py
@@ -1,26 +1,48 @@
 from enum import Enum
+from typing import TypedDict, Optional
 
 
 class ChatGPTModel(Enum):
     GPT_3_5 = "gpt-3.5-turbo"
     GPT_4 = "gpt-4"
 
+
 CHATGPT_ROLE_USER = "user"
 CHATGPT_ROLE_SYSTEM = "system"
 CHATGPT_ROLE_ASSISTANT = "assistant"
+
+
+class ChatGPTFunctionParameterProperty(TypedDict):
+    type: str
+    description: Optional[str]
+    enum: Optional[list[str]]
+
+
+class ChatGPTFunctionParameters(TypedDict):
+    type: str
+    properties: dict[str, ChatGPTFunctionParameterProperty]
+
+
+class ChatGPTFunctionInfo(TypedDict):
+    name: str
+    description: Optional[str]
+    parameters: ChatGPTFunctionParameters
+
 
 class ChatGPTParams:
     def __init__(self,
                  temperature: float | None = None,
                  presence_penalty: float | None = None,
-                 frequency_penalty: float | None = None
+                 frequency_penalty: float | None = None,
+                 functions: list[ChatGPTFunctionInfo] | None = None
                  ):
         self.temperature = temperature
         self.presence_penalty = presence_penalty
         self.frequency_penalty = frequency_penalty
+        self.functions = functions
 
-    def to_params(self)->dict:
-        return {key:value for key, value in self.__dict__.items() if value is not None}
+    def to_params(self) -> dict:
+        return {key: value for key, value in self.__dict__.items() if value is not None}
 
 
 def make_chat_completion_message(message: str, role: str) -> dict:
