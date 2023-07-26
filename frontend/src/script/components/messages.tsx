61c90c5f2ececf5de9cb5ee1eeee8bf95aa6639e
wskSeo <jjang8661@gmail.com>
2023-07-26 16:45:55 +0900

    Upload Persona examples

--- frontend/src/script/components/messages.tsx
+++ frontend/src/script/components/messages.tsx
@@ -1,6 +1,6 @@
 import { ChatMessage } from "../types"
 
-export const MessageView = (props: { message: ChatMessage, overrideMessageText?: string, children?: any }) => {
+export const MessageView = (props: { message: ChatMessage }) =>
     return <div className={`turn-container ${props.message.is_user ? "user" : "system"}`}>
         <img className="profilePic" id = {!props.message.is_user ? "systemPic" : ""} src=""/>
         <div>
