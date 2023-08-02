c99de33bac38c26e9e29b553190f74a467aa3c98
Young-Ho Kim <yghokim@younghokim.net>
2023-08-02 14:22:39 +0900

    Revised label summarizer to be more focused and informative. The next phase decision is made after GPT call programmatically.

--- app/phases/label.py
+++ app/phases/label.py
@@ -1,10 +1,10 @@
 import json
 
-from chatlib.chatbot import DialogueTurn
+from chatlib.chatbot import DialogueTurn, RegenerateRequestException
 from chatlib.chatbot.generators import ChatGPTResponseGenerator
 from chatlib.jinja_utils import convert_to_jinja_template
 # Help the user label their emotion based on the Wheel of Emotions. Empathize their emotion.
-from chatlib.mapper import ChatGPTDialogueSummarizer
+from chatlib.mapper import ChatGPTDialogueSummarizer, ChatGPTDialogSummarizerParams
 from chatlib.openai_utils import ChatGPTParams, ChatGPTModel
 
 from app.common import EmotionChatbotSpecialTokens, PromptFactory
@@ -30,6 +30,7 @@ class WheelOfEmotion:
         ("Contempt", "경멸", "negative"),
         ("Aggressiveness", "공격성", "negative")
     ]
+
 
 # combinations = [
 # ("Anticipation", "Joy", ("Optimism", "낙관")),
@@ -64,108 +65,121 @@ def create_generator():
 - Based on the previous dialog history about the user’s interests, ask them to elaborate more about their emotions and what makes them feel that way.
 - Only when they explicitly mention that they do not know how to describe their emotions or vaguely expressed their emotions (e.g., feels good/bad), tell the user that they can pick as many emotions as they feel at the moment.
 """
-f"""
+                                                                               f"""
     - When you ask the user to pick emotions, append a special token {EmotionChatbotSpecialTokens.EmotionSelect} at the end.
         - With the special token, the user will pick one or more emotions from the list of emotions: {", ".join([f"{eng} ({kor})" for eng, kor, _ in WheelOfEmotion.basics])}.
         - Do not mention the list of emotion words as they will be shown as GUI."""
-"""
-        - The user's choices will be fed as a JSON list, in the format such as [{"key": ...}, {"key":"..."}, ...], where 'key's contain an emotion name.
-    
-- Focus on the user's key episode ("{{key_episode}}") and the emotion about it ("{{user_emotion}}"). 
-- Use only Korean words for the emotions when you mention them in dialogue.
-- Empathize the user's emotion by restating how they felt and share your own experience that is similar to the user's. 
-- If there are multiple emotions, empathize with each one from the user's choices.
-- If the user feels multiple emotions, ask the user how they feel each emotion.
-- If the user's key episode involves other people, ask the user about how the other people would feel.
+                                                                               """
+                                                                                       - The user's choices will be fed as a JSON list, in the format such as [{"key": ...}, {"key":"..."}, ...], where 'key's contain an emotion name.
+                                                                                   
+                                                                               - Focus on the user's key episode ("{{key_episode}}") and the emotion about it ("{{user_emotion}}"). 
+                                                                               - Use only Korean words for the emotions when you mention them in dialogue.
+                                                                               - Empathize the user's emotion by restating how they felt and share your own experience that is similar to the user's. 
+                                                                               - If there are multiple emotions, empathize with each one from the user's choices.
+                                                                               - If the user feels multiple emotions, ask the user how they feel each emotion.
+                                                                               - If the user's key episode involves other people, ask the user about how the other people would feel.
+                                                                               
+                                                                               """ + PromptFactory.get_speaking_rules_block()),
+                                    special_tokens=[
+                                        (EmotionChatbotSpecialTokens.EmotionSelect, "select_emotion", True)])
 
-""" + PromptFactory.get_speaking_rules_block()),
-            special_tokens=[(EmotionChatbotSpecialTokens.EmotionSelect, "select_emotion", True)])
 
-
-summarizer = ChatGPTDialogueSummarizer(
-    base_instruction=convert_to_jinja_template("""
+class LabelSummarizer(ChatGPTDialogueSummarizer):
+    def __init__(self):
+        super().__init__(base_instruction=convert_to_jinja_template("""
 - You are a helpful assistant that analyzes the content of the conversation.
 - Determine whether it is reasonable to move on to the next conversation phase or not.
 - Focus on the user's key episode ("{{key_episode}}") and the emotion about it ("{{user_emotion}}")
 - The user may optionally provide a JSON-formatted list such as '[{"key": ...}, {"key": ...}, ...],'"""f""" where 'key' contains a name of emotion that the user have chosen from a list: {", ".join([f"{eng} ({kor})" for eng, kor, _ in WheelOfEmotion.basics])}.
-"""+"""
+""" + """
 - You must explain and empathize each key (emotion) one by one.
 - Return JSON in the following format:
     {
      "identified_emotion_types": Array<string>,
-     "emotions_assistant_covered": {
-        [key:string]: {"explained": boolean, "empathized": boolean}
-     } // Each emotion in "identified_emotion_types" comes as a key, and a boolean for whether the assistant has covered this emotion in the dialogue.
-     "next_phase": "find" | "record" | null,
-     "rationale": string // rationale for the decision for "next_phase"
+     "emotion_details": {
+        [key:string]: {"reason": string | null, "empathized": boolean, "is_positive": boolean}
+     } // Each emotion in "identified_emotion_types" comes as a key. The "reason" property summarizes the reason of feeling that emotion (null if the user did not explain the reason yet), and "empathized" indicates whether the assistant has empathized this emotion in the dialogue. For "is_positive", set true if the emotion can be classified as a positive one, and false if negative. 
     }
-
-Rules for the "next_phase":
-1) Set null if any of the 'explained' or 'empathized' values in "emotions_assistant_covered" has False value, meaning that the emotions were not sufficiently covered in the conversation.
-2) If all emotions were explained and empathized:
-2-1) Set "find" only when the following conditions are satisfied:
-    - Among the emotions that the user expressed, there is at least one negative emotion that is related to a specific episode that describes problems that the user faced.
-2-2) Set "record" only when the following conditions are satisfied:
-    - The user expressed positive emotions and shared a specific episode when they felt the emotions.
 
 Refer to the examples below.
 """),
 
-    examples=[
-        ([
-             DialogueTurn("어제 학교 쉬는 시간이 낮잠을 자는데 친구가 갑자기 큰 소리를 내서 잠을 못 잤어.", is_user=True),
-             DialogueTurn("그랬구나. 그때 기분이 어땠어?", is_user=False),
-             DialogueTurn("그냥 기분이 안 좋았어", is_user=True),
-             DialogueTurn("어떤 기분이 들었는지 자세히 말해줄 수 있을까?", is_user=False),
-             DialogueTurn("놀라고 화도 났어", is_user=True),
-             DialogueTurn("그랬구나 놀라고 화가 많이 났구나. 어떤 상황에서 놀랐어?", is_user=False),
-             DialogueTurn("내 바로 옆에서 갑자기 큰소리가 나 놀랐어", is_user=True),
-             DialogueTurn("그랬구나 그래서 놀랐구나. 그러면 어떤 상황에서 화가 났어?", is_user=False),
-             DialogueTurn("그 친구가 사과도 없이 계속 시끄럽게 해서 화가 났어", is_user=True),
-             DialogueTurn("사과를 안 해서 화가 났구나.", is_user=False),
-         ],
-         json.dumps({
-             "identified_emotion_types": ["Surprise", "Anger"],
-             "emotions_assistant_covered": {
-                 "Surprise": {"explained": True, "empathized": True},
-                 "Anger": {"explained": True, "empathized": True}
-             },
-             "next_phase": "find",
-             "rationale": "The assistant empathized and explained about all the emotions identified. Since Anger is a negative emotion, the next phase would be 'find'."
-         })),
-                 ([
-             DialogueTurn("어떤 기분이 들었는지 자세히 말해줄 수 있을까?", is_user=False),
-             DialogueTurn("슬프고 후회돼", is_user=True),
-             DialogueTurn("그랬구나 슬프고 후회됐구나. 어떤 상황이 슬펐어?", is_user=False),
-             DialogueTurn("달리기 연습을 많이 했는데 넘어져서 꼴등을 한게 슬퍼", is_user=True),
-             DialogueTurn("그랬구나 그래서 슬펐구나.", is_user=False),
-         ],
-         json.dumps({
-             "identified_emotion_types": ["Sadness", "Remorse"],
-             "emotions_assistant_covered": {
-                 "Sadness": {"explained": True, "empathized": True},
-                 "Remorse": {"explained": False, "empathized": False}
-             },
-             "next_phase": None,
-             "rationale": "Two emotions were identified. But the emotion Remorse has not sufficiently covered in the dialogue. So it's better to keep the conversation."
-         })),
-        ([
-             DialogueTurn("어제 숙제를 다 못 해서 옆에 친구 숙제를 배꼈어.", is_user=True),
-             DialogueTurn("숙제를 못 해서 그랬었구나. 기분이 어땠어?", is_user=False),
-             DialogueTurn("기분이 안 좋았어", is_user=True),
-             DialogueTurn("어떤 기분이 들었는지 자세히 말해줄 수 있을까?", is_user=False),
-             DialogueTurn("뭔가 후회돼", is_user=True),
-             DialogueTurn("친구 숙제를 배낀게 후회가 되는구나.", is_user=False),
-         ],
-         json.dumps({
-             "identified_emotion_types": ["Remorse"],
-             "emotions_assistant_covered": {
-                 "Remorse": {'explained': True, 'empathized': True}
-             },
-             "next_phase": "find",
-             "retionale": "The user expressed a negative emotion remorse, and the assistant empathized that emotion. Also, the user explained why they felt remorse. So it's reasonable to go to 'find' phase."
-         })),
-    ],
-    model=ChatGPTModel.GPT_4_latest,
-    gpt_params=ChatGPTParams(temperature=0.5)
-)
+                         examples=[
+                             ([
+                                  DialogueTurn("어제 학교 쉬는 시간이 낮잠을 자는데 친구가 갑자기 큰 소리를 내서 잠을 못 잤어.", is_user=True),
+                                  DialogueTurn("그랬구나. 그때 기분이 어땠어?", is_user=False),
+                                  DialogueTurn("그냥 기분이 안 좋았어", is_user=True),
+                                  DialogueTurn("어떤 기분이 들었는지 자세히 말해줄 수 있을까?", is_user=False),
+                                  DialogueTurn("놀라고 화도 났어", is_user=True),
+                                  DialogueTurn("그랬구나 놀라고 화가 많이 났구나. 어떤 상황에서 놀랐어?", is_user=False),
+                                  DialogueTurn("내 바로 옆에서 갑자기 큰소리가 나 놀랐어", is_user=True),
+                                  DialogueTurn("그랬구나 그래서 놀랐구나. 그러면 어떤 상황에서 화가 났어?", is_user=False),
+                                  DialogueTurn("그 친구가 사과도 없이 계속 시끄럽게 해서 화가 났어", is_user=True),
+                                  DialogueTurn("사과를 안 해서 화가 났구나.", is_user=False),
+                              ],
+                              json.dumps({
+                                  "identified_emotion_types": ["Surprise", "Anger"],
+                                  "emotion_details": {
+                                      "Surprise": {"reason": "The user suddenly heard a loud noise beside.",
+                                                   "empathized": True, "is_positive": True},
+                                      "Anger": {
+                                          "reason": "The user felt angry because the friend kept making noise without apology.",
+                                          "empathized": True, "is_positive": False}
+                                  }
+                              })),
+                             ([
+                                  DialogueTurn("어떤 기분이 들었는지 자세히 말해줄 수 있을까?", is_user=False),
+                                  DialogueTurn("슬프고 후회돼", is_user=True),
+                                  DialogueTurn("그랬구나 슬프고 후회됐구나. 어떤 상황이 슬펐어?", is_user=False),
+                                  DialogueTurn("달리기 연습을 많이 했는데 넘어져서 꼴등을 한게 슬퍼", is_user=True),
+                                  DialogueTurn("그랬구나 그래서 슬펐구나.", is_user=False),
+                              ],
+                              json.dumps({
+                                  "identified_emotion_types": ["Sadness", "Remorse"],
+                                  "emotion_details": {
+                                      "Sadness": {
+                                          "reason": "The user was sad because they ended up the race in last place even after a lot of practice of running.",
+                                          "empathized": True, "is_positive": False},
+                                      "Remorse": {"reason": None, "empathized": False, "is_positive": False}
+                                  }
+                              })),
+                             ([
+                                  DialogueTurn("어제 숙제를 다 못 해서 옆에 친구 숙제를 배꼈어.", is_user=True),
+                                  DialogueTurn("숙제를 못 해서 그랬었구나. 기분이 어땠어?", is_user=False),
+                                  DialogueTurn("기분이 안 좋았어", is_user=True),
+                                  DialogueTurn("어떤 기분이 들었는지 자세히 말해줄 수 있을까?", is_user=False),
+                                  DialogueTurn("뭔가 후회돼", is_user=True),
+                                  DialogueTurn("친구 숙제를 배낀게 후회가 되는구나.", is_user=False),
+                              ],
+                              json.dumps({
+                                  "identified_emotion_types": ["Remorse"],
+                                  "emotion_details": {
+                                      "Remorse": {
+                                          'reason': "The user was regretful because they copied the friend's homework.",
+                                          'empathized': True, "is_positive": False}
+                                  }
+                              })),
+                         ],
+                         model=ChatGPTModel.GPT_4_latest,
+                         gpt_params=ChatGPTParams(temperature=0.5))
+
+    def _postprocess_chatgpt_output(self, output: str, params: ChatGPTDialogSummarizerParams | None = None) -> dict:
+        result = super()._postprocess_chatgpt_output(output, params)
+        try:
+            if len(result["identified_emotion_types"]) > 0:
+                emotion_infos = [result["emotion_details"][emotion] for emotion in result["identified_emotion_types"]]
+                if len([em for em in emotion_infos if em["reason"] is None or em["empathized"] == False]) > 0:
+                    result["next_phase"] = None
+                    return result
+                else:
+                    result["next_phase"] = "find" if len(
+                        [em for em in emotion_infos if em["is_positive"] == False]) > 0 else "record"
+                    return result
+            else:
+                result["next_phase"] = None
+                return result
+        except:
+            raise RegenerateRequestException("Malformed data.")
+
+
+summarizer = LabelSummarizer()
