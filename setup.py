52490fed6123af02972edc304bad3c5dabba7e3a
Young-Ho Kim <yghokim@younghokim.net>
2025-09-21 03:04:44 +0900

    Fixed outdated import path in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from os import path, getcwd
 
 from chatlib.global_config import GlobalConfig
-from chatlib.integration.openai_api import GPTChatCompletionAPI
+from chatlib.llm.integration import GPTChatCompletionAPI
 from dotenv import find_dotenv, set_key
 
 #Create env files if not exist ========================================================
