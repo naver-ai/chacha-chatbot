342453b07eb2bd2464368d3b3e36952cc7082b84
Young-Ho Kim <yghokim@younghokim.net>
2025-11-13 22:51:38 +0900

    Changed ChaCha logo.

--- frontend/src/script/app/chat/components/IntroFormFrame.tsx
+++ frontend/src/script/app/chat/components/IntroFormFrame.tsx
@@ -1,11 +1,8 @@
 export const IntroFormFrame = (props: {children?: any}) => {
     return <div className="flex flex-col justify-between h-screen intro-form-frame">
-    <div className="my-auto mx-auto chachaContainer">
-        <div className="chachaTextBox">
-            <span>ChaCha</span>
-        </div>
-        <img src={require('../../../../CHACHA.png')}/>
+    <div className="my-auto mx-auto flex flex-col items-center gap-y-4">
+        <img src={require('../../../../chacha_logo.svg')} width={120}/>
+        {props.children}
     </div>
-    {props.children}
     </div>
 }
