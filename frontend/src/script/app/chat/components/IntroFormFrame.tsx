7b69a775f066ec4897b6999ea57f686b036a652d
Young-Ho Kim <yghokim@younghokim.net>
2025-11-13 23:34:25 +0900

    Added title.

--- frontend/src/script/app/chat/components/IntroFormFrame.tsx
+++ frontend/src/script/app/chat/components/IntroFormFrame.tsx
@@ -1,6 +1,7 @@
 export const IntroFormFrame = (props: {children?: any}) => {
     return <div className="flex flex-col justify-between h-screen intro-form-frame">
     <div className="my-auto mx-auto flex flex-col items-center gap-y-4">
+        <div className="text-xl font-black">어린이들을 위한 챗봇, 차차 (HCX)</div>
         {props.children}
     </div>
     </div>
